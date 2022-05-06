8353f5890a251b78de58e4190877856fbf35e8b1
Ajay Kumar Sammangi <ajay.sammangi@mobigesture.com>
2022-05-06 17:08:29 +0530

    react-task1

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    // debugger;
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    // debugger;
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    // debugger;
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    // console.log("this:",this);
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={
+            // console.log({startingArray : this.state.monsters});
+            // console.log(event.target.value);
+            onSearchChange
+          }
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+  
+export default App;
